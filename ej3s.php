addf463c3b1f56f019c52063482dfa1cdf11394a
Andr3wGC <97702693+Andr3wGC@users.noreply.github.com>
2022-09-23 10:59:42 +0200

    Update ej3s.php

--- ej3s.php
+++ ej3s.php
@@ -2,7 +2,7 @@
 <HEAD><TITLE> EJ2-Direccion Red – Broadcast y Rango</TITLE></HEAD>
 <BODY>
 <?php
-$ip="192.168.16.100/31";
+$ip="192.168.16.100/32";
 $punto=".";
 $slash="/";
 $ip1= substr($ip,(strpos($ip,$punto)+1) );
@@ -28,27 +28,79 @@ echo "";
 
 echo "El numero de Direccion de red de $ip es ";
 if($Nred2>=1){
-	echo decbin($ip);
+	$ip = decbin($ip);
+	echo $ip;
 }else{
-	$ipRed=substr($ip,(strpos($ip)+$Nred2) );
+	$ip =decbin($ip);
+	$ip=substr($ip,(0),+$Nred1);
+	echo $ip;
+	$cero=0;
+	for($x=1;$x<=8-$Nred1;$x++){
+	echo $cero;
+	}
 }
 echo ".";
 if($Nred2>=2){
-	echo decbin($ip1);
+	$ip1 = decbin($ip1);
+	echo $ip1;
+}elseif($Nred2==1){
+	$ip1 =decbin($ip1);
+	$ip1=substr($ip1,(0),+$Nred1);
+	$cero="0";
+	for($x=1;$x<=8-$Nred1;$x++){
+	$ip1="$ip1$cero";
+	}
+	echo $ip1;
+}elseif($Nred2<1){
+	$ip1="00000000";
+	echo $ip1;
 }
+
 echo ".";
 if($Nred2>=3){
-	echo decbin($ip2);
+	$ip2 = decbin($ip2);
+	echo $ip2;
+}elseif($Nred2==2){
+	$ip2 =decbin($ip2);
+	$ip2=substr($ip2,(0),+$Nred1);
+	$cero="0";
+	for($x=1;$x<=8-$Nred1;$x++){
+	$ip2="$ip2$cero";
+	}
+	echo $ip2;
+}elseif($Nred2<2){
+	$ip2="00000000";
+	echo $ip2;
 }
+
 echo ".";
 if($Nred2>=4){
-	echo decbin($ip3);
+	$ip3 = decbin($ip3);
+	echo $ip3;
+}elseif($Nred2==3){
+	$ip3 =decbin($ip3);
+	$ip3=substr($ip3,(0),+$Nred1);
+	$cero="0";
+	for($x=1;$x<=8-$Nred1;$x++){
+	$ip3="$ip3$cero";
+	}
+	echo $ip3;
+}elseif($Nred2<3){
+	$ip3="00000000";
+	echo $ip3;
 }
+echo "<br><br>";
+echo bindec("$ip");
+echo ".";
+echo bindec("$ip1");
+echo ".";
+echo bindec("$ip2");
+echo ".";
+echo bindec("$ip3");
 
 
 
-//echo ".";
-//printf("%b",$ip1);
+
 
 echo "<br>";
 
